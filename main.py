13c6acfe7aae04d5ad0b3ba91af16b7d9e9ee9a7
almas yaar khan <almasyarkhan007@gmail.com>
2023-09-07 16:26:45 +0530

    builtnewfile

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from sched import scheduler
+import time
 from fastapi import FastAPI
 import uvicorn
 from dbmanager import db_connect 
+import pandas
 
 app = FastAPI()
 db = db_connect()
@@ -11,8 +14,26 @@ def import_data():
     pass
 
 
+@app.get("/build")
+def build_results():
+    df = {"key":"value"}
+    df = pandas.read_csv("api.csv").to_dict(orient='records')
 
-
+    return df
 
 if __name__ == "__main__":
     uvicorn.run("main:app",port=8001,reload=True)
+
+@app.get("/buildtxt")
+def check_for_new_files(): 
+ def save_data_to_mongodb_and_remove(file_path):
+    while True:
+        scheduler.run_pending()
+        time.sleep(1)
+    
+
+    
+
+
+
+
